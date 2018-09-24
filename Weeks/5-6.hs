096fb39f3f7207b4b6c3ff0eedd358444bdf512d
h181189 <h181189@stud.hvl.no>
2018-09-24 15:49:52 +0200

    Solve week 5-6

--- Weeks/5-6.hs
+++ Weeks/5-6.hs
@@ -1,6 +1,7 @@
 -- 3
 data Exp = S | U | Og Exp Exp | El Exp Exp | Ik Exp
 
+    -- a
 folde :: a -> a -> (a -> a -> a) -> (a -> a -> a) -> (a -> a) -> Exp -> a
 folde s u o e i S = s
 folde s u o e i U = u
@@ -8,5 +9,16 @@ folde s u o e i (Og x y) = o (folde s u o e i x) (folde s u o e i y)
 folde s u o e i (El x y) = e (folde s u o e i x) (folde s u o e i y)
 folde s u o e i (Ik x) = i (folde s u o e i x)
 
+    -- b
 evb :: Exp -> Bool
 evb exp = folde True False (&&) (||) (not) exp
+
+    -- c
+evi :: Exp -> Int
+evi exp = folde 1 5 (+) (*) negate exp
+
+    -- d
+succMax x y = succ (max x y) 
+
+evh :: Exp -> Int
+evh exp = folde 1 1 succMax succMax succ exp
